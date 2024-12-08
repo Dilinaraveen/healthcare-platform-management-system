d3405b3c58adfd211f6aff0672cae75e7ad4f555
Dilina Raveen <dilinaraveen@gmail.com>
2024-12-08 09:47:55 +0530

    Dec 8 changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,25 +5,34 @@ import { useContext } from "react";
 import Navbar from "./components/Navbar";
 import { AdminContext } from "./context/AdminContext";
 import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
 
 function App() {
   const { aToken } = useContext(AdminContext);
 
   console.log(aToken)
 
-  return aToken ? (
+  return (
     <div className="bg-[f8f9fd]">
       <ToastContainer />
-      <Navbar />
-      <div className="flex items-start"> 
-        <Sidebar />
+      {aToken && <Navbar /> }
+      <div className={aToken ? 'flex items-start' : ''}> 
+        {aToken && <Sidebar />}
+        <Routes>
+          <Route path="/" element={aToken ?<></> : <Login/>} />
+          <Route path="/admin-dashboard" element={<Dashboard/>} />
+          <Route path="/all-appointments" element={<AllAppointments/>} />
+          <Route path="/add-doctor" element={<AddDoctor/>} />
+          <Route path="/doctor-list" element={<DoctorsList/>} />
+          <Route path="/login" element={<Login/>} />
+
+        </Routes>
       </div>
     </div>
-  ) : (
-    <>
-      <Login />
-      <ToastContainer />
-    </>
   )
 }
 
