e334ea2e815a4e929dd93f3a2ecbc2ae1f3a31a0
Dilinaraveen <dilinaraveen@gmail.com>
2025-04-19 20:44:02 +0530

    admin-dashboard completed

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,6 +11,7 @@ const AdminContextProvider = (props) => {
   );
   const [doctors, setDoctors] = useState([]);
   const [appointments, setAppointments] = useState([]);
+  const [dashData, setDashData] = useState(false);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -83,6 +84,22 @@ const AdminContextProvider = (props) => {
     }
   }
 
+  const getDashData = async () => {
+    try{
+      const { data } = await axios.get(
+        backendUrl + "/api/admin/dashboard",
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        setDashData(data.dashData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
 
 
   const value = {
@@ -94,7 +111,9 @@ const AdminContextProvider = (props) => {
     changeAvailability,
     appointments, setAppointments,
     getAllAppointments,
-    cancelAppointment
+    cancelAppointment,
+    dashData,setDashData,
+    getDashData
   };
 
   return (
--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import {v2 as cloudinary} from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import appointmentModel from "../models/appointmentModel.js";
 
@@ -139,4 +140,26 @@ const cancelAppointment = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, cancelAppointment };
+//API to get dashbaord data for admin panel
+const adminDashboard = async (req,res) => {
+    try {
+        const doctors = await doctorModel.find({});
+        const users = await userModel.find({});
+        const appointments = await appointmentModel.find({});
+        
+        const dashData = {
+            doctors: doctors.length,
+            patients: users.length,
+            appointments: appointments.length,
+            latestAppointments: appointments.reverse().slice(0, 5),
+        }
+        
+        res.json({ success: true, dashData });
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, cancelAppointment, adminDashboard };
