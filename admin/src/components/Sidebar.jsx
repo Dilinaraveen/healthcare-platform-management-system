a20f7b54f6393769df08d9ae1382a10db422c40c
Dilinaraveen <dilinaraveen@gmail.com>
2025-04-22 14:54:30 +0530

    doctors-appointment - api integration

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
             to={"/admin-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
 
           <NavLink
@@ -34,7 +34,7 @@ const Sidebar = () => {
             to={"/all-appointments"}
           >
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointment</p>
+            <p className="hidden md:block">Appointment</p>
           </NavLink>
 
           <NavLink
@@ -46,7 +46,7 @@ const Sidebar = () => {
             to={"/add-doctor"}
           >
             <img src={assets.add_icon} alt="" />
-            <p>Add Doctor</p>
+            <p className="hidden md:block">Add Doctor</p>
           </NavLink>
 
           <NavLink
@@ -58,7 +58,7 @@ const Sidebar = () => {
             to={"/doctor-list"}
           >
             <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
+            <p className="hidden md:block">Doctors List</p>
           </NavLink>
         </ul>
       )}
@@ -73,7 +73,7 @@ const Sidebar = () => {
             to={"/doctor-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
 
           <NavLink
@@ -85,7 +85,7 @@ const Sidebar = () => {
             to={"/doctor-appointments"}
           >
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointment</p>
+            <p className="hidden md:block">Appointment</p>
           </NavLink>
 
           <NavLink
@@ -97,7 +97,7 @@ const Sidebar = () => {
             to={"/doctor-profile"}
           >
             <img src={assets.people_icon} alt="" />
-            <p>Profile</p>
+            <p className="hidden md:block">Profile</p>
           </NavLink>
 
     
--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 
@@ -8,10 +10,59 @@ const DoctorContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const [dToken, setDToken] = useState(localStorage.getItem("dToken") ? localStorage.getItem("dToken") : '');
+    const [appointments, setAppointments] = useState([]);
+
+    const getAppointments = async () => {
+      try {
+
+        const {data} = await axios.get(backendUrl +'/api/doctor/appointments', {headers: {dToken}});
+        if(data.success) {
+          setAppointments(data.appointments);
+        } else {
+          toast.error(data.message);
+        }
+      } catch (error) {
+        console.log(error.message);
+        toast.error(error.message);
+      }
+    }
+
+    const completeAppointment = async (appointmentId) => {
+      try {
+        const {data} = await axios.post(backendUrl +'/api/doctor/complete-appointment', {appointmentId}, {headers: {dToken}});
+        if(data.success) {
+          toast.success(data.message);
+          getAppointments();
+        } else {
+          toast.error(data.message);
+        }
+      } catch (error) {
+        console.log(error.message);
+        toast.error(error.message);
+      }
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+      try {
+        const {data} = await axios.post(backendUrl +'/api/doctor/cancel-appointment', {appointmentId}, {headers: {dToken}});
+        if(data.success) {
+          toast.success(data.message);
+          getAppointments();
+        } else {
+          toast.error(data.message);
+        }
+      } catch (error) {
+        console.log(error.message);
+        toast.error(error.message);
+      }
+    }
 
     const value = {
       dToken, setDToken,
       backendUrl,
+      appointments, setAppointments,
+      getAppointments,
+      completeAppointment, cancelAppointment
     };
 
   return (
