4e1d09e5f759325ef7c7ae4379ad33ad723bc673
Dilinaraveen <dilinaraveen@gmail.com>
2025-04-24 19:49:56 +0530

    scheduler bug fixed

--- api/config/reminderScheduler.js
+++ api/config/reminderScheduler.js
@@ -24,6 +24,7 @@ const scheduleReminders = () => {
       const allAppointments = await appointmentModel.find({
         slotDate: formattedToday,
         cancelled: false,
+        reminderSent: false,
         isCompleted: false,
       });
       
